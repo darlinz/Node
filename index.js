554169b33ab5d4f84f6be794a20cc066bc704daa
Eva <Otvratitelno00@gmail.com>
2024-11-19 19:32:12 +0100

    Init

---
+++ index.js
@@ -0,0 +1,5 @@
+const obj = require("./user")
+console.log(obj.user)
+obj.sayHello()
+console.log("work dir", __dirname)
+console.log("file name", __filename)
---
+++ user.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'Mori',
+    age: 21
+}
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log("Hallo!!")
+    }
+}
