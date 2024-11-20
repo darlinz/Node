fe553164d7814a82638f81e5b1481ae81e60aff3
Eva <Otvratitelno00@gmail.com>
2024-11-20 17:29:54 +0100

    Simple Web-server

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const http = require('http'); 
-const app = require('./App'); 
+const http = require("http");
 
-const PORT = 3000; 
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text" />
+                <button type="submit">Send</button>
+            </form>
+        `);
+    } else if (req.method === 'POST') {
+        const body = [];
 
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
 
-const server = http.createServer(app);
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
 
-server.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}. Перейдите по адресу http://localhost:${PORT}`);
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = new URLSearchParams(parsedBody).get('title'); // Парсинг данных формы
+
+            res.end(`
+                <h1>Ваше сообщение: ${decodeURIComponent(message)}</h1>
+            `);
+        });
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000...');
 });
